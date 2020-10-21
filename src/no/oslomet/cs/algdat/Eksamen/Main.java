766088dd163ab979f4b6cd4e4487188cbf9d4606
Camilla92 <44602961+Camilla92@users.noreply.github.com>
2020-10-21 14:24:55 +0200

    Lagt til kopi av kode 5.2.3 a og lagt inn test koden i main

--- src/no/oslomet/cs/algdat/Eksamen/Main.java
+++ src/no/oslomet/cs/algdat/Eksamen/Main.java
@@ -15,5 +15,17 @@ public class Main {
 
         EksamenSBinTre<Integer> tre3= new EksamenSBinTre<>(Comparator.naturalOrder());
         System.out.println(tre3.antall());
+
+
+        //test for oppgave 1:
+
+        Integer[] a = {4,7,2,9,5,10,8,1,3,6};
+        EksamenSBinTre<Integer> tre4= new EksamenSBinTre<>(Comparator.naturalOrder());
+        for(int verdi:a){
+
+            tre4.leggInn(verdi);
+        }
+        System.out.println(tre.antall()); //utskrift 10
+
     }
 }
