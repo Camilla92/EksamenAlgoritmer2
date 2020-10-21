6e0ecd764e28eb32e4bf5f33471eca417f721a9a
Camilla92 <44602961+Camilla92@users.noreply.github.com>
2020-10-21 15:02:59 +0200

    lagt inn noen endringer

--- src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -88,28 +88,32 @@ public class EksamenSBinTre<T> {
         //kopi av kode 5.2.3
         Objects.requireNonNull(verdi, "Ulovlig med nullverdier!");
 
-        Node<T> p = rot, q = null;               // p starter i roten
+        Node<T> p = rot, forelder = null;               // p starter i roten
         int cmp = 0;                             // hjelpevariabel
 
         while (p != null)       // fortsetter til p er ute av treet
         {
-            q = p;                                 // q er forelder til p
+            forelder = p;                                 // q er forelder til p
             cmp = comp.compare(verdi,p.verdi);     // bruker komparatoren
             p = cmp < 0 ? p.venstre : p.høyre;     // flytter p
         }
 
         // p er nå null, dvs. ute av treet, q er den siste vi passerte
 
-        p = new Node<>(verdi);                   // oppretter en ny node
+        p = new Node<T>(verdi, null, null, forelder);                   // oppretter en ny node
 
-        if (q == null) rot = p;                  // p blir rotnode
-        else if (cmp < 0) q.venstre = p;         // venstre barn til q
-        else q.høyre = p;                        // høyre barn til q
+        if (forelder == null) {
+            rot = p;// p blir rotnode
+        }
+        else if (cmp < 0){
+            forelder.venstre = p; // venstre barn til q
+        }
+        else {
+            forelder.høyre = p;  // høyre barn til q
+        }
 
         antall++;                                // én verdi mer i treet
         return true;                             // vellykket innlegging
-
-        //throw new UnsupportedOperationException("Ikke kodet ennå!");
     }
 
     public boolean fjern(T verdi) {
