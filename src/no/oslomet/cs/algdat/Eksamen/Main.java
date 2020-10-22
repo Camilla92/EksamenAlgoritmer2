dc8817aa5a17103c5c2267cb25a07db810adacf6
Camilla92 <44602961+Camilla92@users.noreply.github.com>
2020-10-22 10:02:28 +0200

    Fikset oppgave 1

--- src/no/oslomet/cs/algdat/Eksamen/Main.java
+++ src/no/oslomet/cs/algdat/Eksamen/Main.java
@@ -18,6 +18,7 @@ public class Main {
 
 
         //test for oppgave 1:
+        System.out.println("Opgpave 1.");
 
         Integer[] a = {4,7,2,9,5,10,8,1,3,6};
         EksamenSBinTre<Integer> tre4= new EksamenSBinTre<>(Comparator.naturalOrder());
